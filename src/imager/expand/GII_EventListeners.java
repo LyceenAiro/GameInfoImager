7c82c4fe4ded8e729cf79c1649dbca7a66439cca
LyceenAiro <1534012710@qq.com>
2024-05-09 09:49:23 +0800

    v1.5_7

--- src/imager/expand/GII_EventListeners.java
+++ src/imager/expand/GII_EventListeners.java
@@ -157,7 +157,7 @@ public class GII_EventListeners{
 	
 	public static int lastID;
 	
-	public static Interval timer = new Interval(2);
+	public static Interval timer = new Interval(1);
 	
 	public static TaskQueue taskQueue = new TaskQueue();
 	public static Thread updateThread;
@@ -237,27 +237,27 @@ public class GII_EventListeners{
 			
 			GII_Plugin.showHealthBar = Core.settings.getBool(GII_Plugin.SHOW_UNIT_HEALTH_BAR, true);
 			
-			if(timer.get(12f)){
+//			if(timer.get(12f)){
+//				taskQueue.post(() -> {
+//					synchronized(unitsUTD){
+//						unitsUTD.clear();
+//						Groups.unit.copy(unitsUTD).filter(addUnit);
+//						units = new Seq<>(unitsUTD);
+//					}
+//				});
+//
+//				UnitInfo.update();
+//			}
+			
+			if(timer.get(1, 12f)){
 				taskQueue.post(() -> {
-					synchronized(unitsUTD){
-						unitsUTD.clear();
-						Groups.unit.copy(unitsUTD).filter(addUnit);
-						units = new Seq<>(unitsUTD);
+					synchronized(buildsUTD){
+						buildsUTD.clear();
+						Groups.build.copy(buildsUTD).filter(addBuilding);
+						builds = new Seq<>(buildsUTD);
 					}
 				});
-
-				UnitInfo.update();
 			}
-			
-			// if(timer.get(1, 30f)){
-			// 	taskQueue.post(() -> {
-			// 		synchronized(buildsUTD){
-			// 			buildsUTD.clear();
-			// 			Groups.build.copy(buildsUTD).filter(addBuilding);
-			// 			builds = new Seq<>(buildsUTD);
-			// 		}
-			// 	});
-			// }
 		});
 		
 		Events.on(EventType.TilePreChangeEvent.class, e -> {
